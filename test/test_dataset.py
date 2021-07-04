06c12d591b9491c520380762849e14dc184eb21e
yamaneco28 <ykouki111228@gmail.com>
2021-07-04 22:18:33 +0900

    add dataset test

---
+++ test/test_dataset.py
@@ -0,0 +1,30 @@
+import unittest
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import sys
+sys.path.append('.')
+from datasets.mvtec import MVTecDataset
+
+
+class TestMyDataset(unittest.TestCase):
+    def test_dataset(self):
+        datafolder = 'data/mvtec'
+        dataset = MVTecDataset(datafolder)
+        print('length:', len(dataset))
+        dataloader = DataLoader(
+            dataset,
+            batch_size=8,
+            shuffle=True,
+            num_workers=8,
+            pin_memory=True
+        )
+        for (x, y, mask) in tqdm(dataloader):
+            # pass
+            print('x shape:', x.shape)
+            print('y shape:', y.shape)
+            print('mask shape:', mask.shape)
+
+
+if __name__ == "__main__":
+    unittest.main()
